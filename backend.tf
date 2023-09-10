53ecc47fa06dbc992756bba311e80e0328421552
CapstoneProjectItc <140419054+ItcTrainee2023@users.noreply.github.com>
2023-09-11 00:20:42 +0100

    removed tf state file block

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "azurerm" {
-    tenant_id            = "104e77d4-81e7-4c16-ab44-935220bed6dd"
-    subscription_id      = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
-    resource_group_name  = "notifier-resource-group"
-    storage_account_name = "notifiertfstore01"
-    container_name       = "notifiertfstate"
-    key                  = "terraform-notifier.tfstate"
-  }
-}
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>terraform {
+#   backend "azurerm" {
+#     tenant_id            = "104e77d4-81e7-4c16-ab44-935220bed6dd"
+#     subscription_id      = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
+#     resource_group_name  = "notifier-resource-group"
+#     storage_account_name = "notifiertfstore01"
+#     container_name       = "notifiertfstate"
+#     key                  = "terraform-notifier.tfstate"
+#   }
+# }
